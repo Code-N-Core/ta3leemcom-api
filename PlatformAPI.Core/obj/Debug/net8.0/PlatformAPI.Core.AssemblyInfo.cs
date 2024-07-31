7905bb9645b3e01253f545978d3ab11948d07e7a
ahmed06610 <ahmedalaraby066@gmail.com>
2024-08-01 02:18:22 +0300

    ok

--- PlatformAPI.Core/obj/Debug/net8.0/PlatformAPI.Core.AssemblyInfo.cs
+++ PlatformAPI.Core/obj/Debug/net8.0/PlatformAPI.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformAPI.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4fd2f65eb0edfd58f5bb184f6297a8503a4a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa83acdb321ac820a2db0024133a03698b7a69d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformAPI.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformAPI.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

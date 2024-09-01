1864c9c9e032dd6761f5b2410df8e0f380200dbb
Sherif Ebrahim <90417294+sherif2611@users.noreply.github.com>
2024-09-01 17:19:08 +0300

    test

---
+++ PlatformAPI.Core/DTOs/Day/DayDTO.cs
@@ -0,0 +1,12 @@
+﻿using PlatformAPI.Core.Models;
+
+namespace PlatformAPI.Core.DTOs.Day
+{
+    public class DayDTO
+    {
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int MonthId { get; set; }
+    }
+}
---
+++ PlatformAPI.Core/DTOs/Teacher/UpdateTeacherDTO.cs
@@ -0,0 +1,14 @@
+﻿using PlatformAPI.Core.CustomValidation;
+
+namespace PlatformAPI.Core.DTOs.Teacher
+{
+    public class UpdateTeacherDTO
+    {
+        [Required]
+        public int TeacherId { get; set; }
+        [Required,FullName]
+        public string TeacherName { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}

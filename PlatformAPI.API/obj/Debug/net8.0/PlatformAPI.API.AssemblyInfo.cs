c6ecb4fe471d2857bc0266e66c24f92be332cc53
Sherif Ebrahim <90417294+sherif2611@users.noreply.github.com>
2024-09-11 14:58:47 +0300

    save

--- PlatformAPI.API/obj/Debug/net8.0/PlatformAPI.API.AssemblyInfo.cs
+++ PlatformAPI.API/obj/Debug/net8.0/PlatformAPI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformAPI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b422f424148ffafa36698254bf9a1f50a1a863c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformAPI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformAPI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

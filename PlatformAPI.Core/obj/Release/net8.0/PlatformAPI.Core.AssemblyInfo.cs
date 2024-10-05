53a09c5684aeb732df48c244658177ab53d1c4f9
Sherif Ebrahim <90417294+sherif2611@users.noreply.github.com>
2024-10-05 23:34:45 +0300

    saveLastChangesForStdMonth

--- PlatformAPI.Core/obj/Release/net8.0/PlatformAPI.Core.AssemblyInfo.cs
+++ PlatformAPI.Core/obj/Release/net8.0/PlatformAPI.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformAPI.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce0ff1896c263c1c5b6e89f576d46334ebe43be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d962dfaa64c353b90da55b102c4ee4526b20a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformAPI.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformAPI.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
